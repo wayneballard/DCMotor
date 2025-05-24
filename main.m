13f821c49cc853cb60adef32b4fdbec8a64dd630
wayneballard <126793814+wayneballard@users.noreply.github.com>
2025-05-24 22:20:04 +0200

    Add files via upload

---
+++ main.m
@@ -0,0 +1,24 @@
+%close all;clear;clc;
+
+tl=-10:2:10;
+km=1.5; ke=0.05;
+V=100;R=5;
+
+TL=2;
+kp=1.5;
+OM=50;
+
+L=0.05;
+J=0.05;
+
+
+current_time = out.ISE_current.Time
+current_data = out.ISE_current.Data
+speed_time = out.ISE_speed.Time
+speed_data = out.ISE_speed.Data
+torque = out.torque.Data
+
+
+
+
+
