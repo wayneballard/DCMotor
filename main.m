067af9c377a531e79f594f97c527e3d3f48b3d48
wayneballard <126793814+wayneballard@users.noreply.github.com>
2025-10-17 19:02:43 +0200

    Update main.m

--- main.m
+++ main.m
@@ -11,7 +11,7 @@ J=0.05;
 
 T_p = 0.01
 
-Kb = 18
+Kb = 20
 Tii= L/R
 Kpi = Tii/(2*(1/R)*T_p)
 Kii = Kpi/Tii
@@ -20,12 +20,6 @@ Tiw = 4*2*T_p
 Kpw = J/(2*kphi*2*T_p)
 Kiw = Kpw/Tiw
 
-
-current_time = out.ISE_current.Time
-current_data = out.ISE_current.Data
-speed_time = out.ISE_speed.Time
-speed_data = out.ISE_speed.Data
-torque = out.torque.Data
 
 
 
